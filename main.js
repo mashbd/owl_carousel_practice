5ad23dabd9091a1167f33f7fcffb97a2dc643b5c
Arshad Sarkar Hanif <65861090+mashbd@users.noreply.github.com>
2021-07-26 17:44:09 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    time:1000,
+    autoplay:true,
+    autoplayTimeout:2000,
+    stagePadding: 50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
